d68ade4d19d2ebe71f9799fe4bf76fadc97f49f9
cbastable <therightstuff17@gmail.com>
2013-06-20 21:43:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conradbastable::Application.config.secret_token = 'a96e6ea55206b6993fe991262c1747be83c5b0a72448a9ec8147e03d6866ee1e0f32a16ebc3bdf9373da5582e8fbb4786c33a67e7d9cfaa59c1e950fcda20f04'
